ac1b286daa7a26f034c07fb631ff39f2f676c15a
Levit Kanner <lkanner21@gmail.com>
2022-07-23 21:13:06 +0000

    Add text field component

---
+++ src/components/TextField.js
@@ -0,0 +1,11 @@
+const TextField = (props) => {
+    const {label} = props;
+    return (
+        <div className="flex flex-col w-full">
+            <label htmlFor={label} className={'formLabel'}>{label}</label>
+            <input id={label} {...props} className={'formInput'}/>
+        </div>
+    )
+};
+
+export default TextField;

178a8c0dbc2b3c0b1cfd8c5d7f8d05216f116737
Levit Kanner <lkanner21@gmail.com>
2022-07-23 21:13:26 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+
 import './App.css';
+import TextField from "./components/TextField";
+
+const client = new QueryClient();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <QueryClientProvider client={client}>
+            <div className="min-h-screen min-w-full">
+                <TextField label="Username" required/>
+                Home page
+            </div>
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </QueryClientProvider>
+    );
 }
 
 export default App;

ac61fe3e96eb7e3196f9a1ba19420be721a7fc3a
Levit Kanner <lkanner21@gmail.com>
2022-07-30 21:29:02 +0000

    Add custom axios instance

--- src/api/Client.js
+++ src/api/Client.js
@@ -1,14 +1,33 @@
 import axios from "axios";
+import {refresh} from "./Auth";
+
 const client = axios.create({
-    baseUrl: process.env.REACT_APP_BASE_URL,
-    headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}
+    baseUrl: process.env.REACT_APP_BASE_URL
 });
 
+client.interceptors.request.use(config => {
+        config.baseURL = process.env.REACT_APP_BASE_URL
+        config.headers["Authorization"] = `Bearer ${localStorage.getItem('access')}`;
+        return config;
+    },
+    error => Promise.reject(error)
+);
 
-const Login = (payload) => {
-    return client.post('login', payload);
-}
+client.interceptors.response.use(response => response,
+    async error => {
+        const prevRequest = error?.config;
+        if (error?.response?.status === 401) {
+            // send request to get new access token
+            const authTokens = {
+                accessToken: localStorage.getItem('access') ,
+                refreshToken: localStorage.getItem('refresh')
+            }
 
-export const useLogin = (payload) => {
+            const tokens = await refresh(authTokens);
+            client(prevRequest)
+            // prevRequest.headers['Authorization'] = `Bearer ${}`
+        }
+        return Promise.reject(error)
+    })
 
-}
+export default client;

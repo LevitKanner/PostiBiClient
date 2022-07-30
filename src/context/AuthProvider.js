f14e48e6a988f007a3fb92f6920ca8f8a24995d2
Levit Kanner <lkanner21@gmail.com>
2022-07-30 21:27:39 +0000

    Add auth provider context

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,12 @@
+import {createContext, useContext, useState} from "react";
+
+const AuthContext = createContext({})
+export const useAuthContext = () => useContext(AuthContext);
+
+export const AuthProvider = ({children}) => {
+    const [auth, setAuth] = useState({});
+
+    return <AuthContext.Provider value={{auth, setAuth}}>
+        {children}
+    </AuthContext.Provider>
+};

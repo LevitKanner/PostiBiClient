4ec165bb9364eb926cfcb572030158109b7123eb
Levit Kanner <lkanner21@gmail.com>
2022-07-31 21:02:34 +0000

    Configures routing

--- src/index.js
+++ src/index.js
@@ -5,16 +5,26 @@ import App from './App';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./views/Login";
 import Register from "./views/Register";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {AuthProvider} from "./context/AuthProvider";
 
+const client = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App/>}/>
-                <Route path="login" element={<Login/>}/>
-                <Route path="register" element={<Register/>}/>
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={client} contextSharing={true}>
+            <BrowserRouter>
+                <AuthProvider>
+                    <Routes>
+                        <Route path="/*" element={<App/>}/>
+                        <Route path="login" element={<Login/>}/>
+                        <Route path="register" element={<Register/>}/>
+                    </Routes>
+                </AuthProvider>
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </QueryClientProvider>
     </React.StrictMode>
 );

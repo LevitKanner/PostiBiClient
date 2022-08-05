0ac6132fae6c67ee4d5249c674b53c547f7bfdbc
Levit Kanner <lkanner21@gmail.com>
2022-08-05 20:35:40 +0000

    Modify post endpoints

--- src/api/PostApi.js
+++ src/api/PostApi.js
@@ -1,10 +1,9 @@
-// import {privateClient} from "./Client";
-//
-//
-// export const getAllPosts = () => privateClient.get("posts");
-// export const getUserPosts = (userId) => privateClient.get(`posts/user-posts/${userId}`);
-// export const getPost = (postId) => privateClient.get(`posts/${postId}`);
-// export const createPost = (payload, userId) => privateClient.post(`posts?userId=${userId}`, payload);
-// export const updatePost = (payload, postId) => privateClient.put(`posts/${postId}`, payload);
-// export const deletePost = (postId) => privateClient.delete(`posts/${postId}`);
-// export const getFollowingPosts = (userId) => privateClient().get(`posts/user-following/${userId}`);
+import client from "./Client";
+
+export const getAllPosts = () => client.get("posts");
+export const getUserPosts = (userId) => client.get(`posts/user-posts/${userId}`);
+export const getPost = (postId) => client.get(`posts/${postId}`);
+export const createPost = (payload, userId) => client.post(`posts?userId=${userId}`, payload);
+export const updatePost = (payload, postId) => client.put(`posts/${postId}`, payload);
+export const deletePost = (postId) => client.delete(`posts/${postId}`);
+export const getFollowingPosts = (userId) => client.get(`posts/user-following/${userId}`)
